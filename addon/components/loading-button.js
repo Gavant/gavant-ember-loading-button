b475511dc612963d5929556f049f1c135c960aa0
Bill Dami <bdami@gavant.com>
2017-02-13 15:43:40 -0500

    TP-37609: success/error states WIP

--- addon/components/loading-button.js
+++ addon/components/loading-button.js
@@ -2,11 +2,19 @@ import Ember from 'ember';
 import layout from '../templates/components/loading-button';
 
 const {
-  get,
-  set,
-  computed,
-  getWithDefault,
-  Component
+    get,
+    set,
+    computed,
+    computed: { or },
+    getWithDefault,
+    canInvoke,
+    Component,
+    String: { capitalize },
+    RSVP: { Promise },
+    run: {
+        later,
+        cancel
+    }
 } = Ember;
 
 let positionalParamsMixin = {
@@ -15,35 +23,83 @@ let positionalParamsMixin = {
 
 export default Component.extend(positionalParamsMixin, {
     layout: layout,
-    targetObject: computed.alias('parentView.controller') ? computed.alias('parentView.controller') : computed.alias('parentView'),
+    targetObject: or('parentView.controller', 'parentView'),
     tagName: 'button',
     classNames: ['spinner-button', 'btn'],
-    classNameBindings: ['isSpinning'],
+    classNameBindings: ['isSpinning', 'successShown', 'errorShown'],
     attributeBindings: ['disabled', 'title'],
+    loadingClass: 'loading',
     isSpinning: false,
+    showSuccess: true,
+    showError: true,
+    successShown: false,
+    errorShown: false,
+    successStateDuration: 3000,
+    successAnimateInDuration: 1000,
+    errorStateDuration: 1000,
+    errorAnimateInDuration: 1000,
+    successIcon: 'fa fa-check-circle',
 
-    style: computed('width', 'isSpinning', function() {
-        //only set a hard width when the spinner is shown so that the button naturally determines
+    style: computed('width', 'isSpinning', 'successShown', 'errorShown', function() {
+        //only set a hard width when the spinner or result states are shown so that the button naturally determines
         //its width based on its content/container width when in the non spinning state
         //this is mainly is for block-level buttons (.btn-block), whose width can dynamically change in RWD situations
-        let width = get(this, 'width'),
-            string = get(this, 'isSpinning') ? `width:${width}px;` : "";
+        const width = get(this, 'width');
+        const style = get(this, 'isSpinning') || get(this, 'successShown') || get(this, 'errorShown') ? `width:${width}px;` : "";
 
-        return Ember.String.htmlSafe(string);
+        return Ember.String.htmlSafe(style);
     }),
 
-    click: function (event) {
+    showResultState(type, animateInCallback) {
+        if(!this.isDestroyed && get(this, `show${capitalize(type)}`)) {
+            set(this, `${type}Shown`, true);
+
+            const showResultTimer = later(this, () => {
+                if(!this.isDestroyed) {
+                    set(this, `${type}Shown`, false);
+                }
+            }, get(this, `${type}StateDuration`));
+
+            set(this, 'showResultTimer', showResultTimer);
+
+            //send an action when a result state finishes animating in
+            //e.g. so the parent controller can transition once the success state is shown
+            if(typeof animateInCallback === 'function') {
+                later(this, animateInCallback, get(this, `${type}AnimateInDuration`));
+            }
+
+            // if(canInvoke(this.attrs, `on${capitalize(type)}AnimateIn`)) {
+            //     later(this.attrs, `on${capitalize(type)}AnimateIn`, get(this, `${type}AnimateInDuration`));
+            // }
+        }
+    },
+
+    click(event) {
         event.preventDefault();
         let isSpinning = get(this, 'isSpinning');
         if (!isSpinning) {
             this.setProperties({
                 width: this.$('.fixed-width').width(),
-                isSpinning: true
+                isSpinning: true,
+                successShown: false,
+                errorShown: false
             });
-            var params = getWithDefault(this, 'params', []);
 
+            cancel(this.get('showResultTimer'));
+
+            const params = getWithDefault(this, 'params', []);
             //coerce the returned value into an RSVP promise object to ensure it has a .finally() method
-            Ember.RSVP.resolve(this.attrs.clicked(params)).finally(() => {
+            const resolvedPromise = Ember.RSVP.resolve(this.attrs.clicked(params));
+
+            resolvedPromise.then((callback) => {
+                this.showResultState('success', callback);
+            });
+
+            resolvedPromise.catch((callback) => {
+                this.showResultState('error', callback);
+            });
+
+            resolvedPromise.finally(() => {
                 if (!this.isDestroyed) {
                     set(this, 'isSpinning', false);
                 }
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,12 +4,28 @@ export default Ember.Controller.extend({
     actions: {
         clickWithPromise() {
             return new Ember.RSVP.Promise((resolve) => {
-                Ember.run.later(this, resolve, 2000);
+                Ember.run.later(this, () => {
+                    resolve(Ember.run.bind(this, this.nonAnonFunc));
+                }, 1000);
+            });
+        },
+
+        clickWithRejectedPromise() {
+            return new Ember.RSVP.Promise((resolve, reject) => {
+                Ember.run.later(this, () => {
+                    reject(function() {
+                        console.log('error animated in!');
+                    });
+                }, 1000);
             });
         },
 
         clickWithoutPromise() {
             return true;
+        },
+
+        nonAnonFunc(foo) {
+            console.log('some func on the controller!', foo);
         }
     }
 });
